338eac316495d56799f0518de7d30032c0a0b558
Forrest Parker <forrestwparker@gmail.com>
2018-06-18 16:13:46 -0700

    Ex 3.2

--- CtCI Solutions/Solutions/Chapter 3/Ex2.cs
+++ CtCI Solutions/Solutions/Chapter 3/Ex2.cs
@@ -17,7 +17,39 @@ namespace CtCI_Solutions.Solutions
              * 'Push', 'pop', and 'min' should all operate in O(1) time.
              */
 
-            // <Code>
+            // Assuming this stack can be used exclusively for int values.
+            // MyStack.Pop will throw an error if empty.
+            public class MyStack
+            {
+                // Stack holds the stack.
+                // MinStack holds copies of the minimum elements in Stack (by order of appearance).
+                private Stack<int> Stack = new Stack<int>();
+                private Stack<int> MinStack = new Stack<int>();
+
+                // value is pushed on MinStack if it is less than or equal to the current minimum of Stack.
+                public void Push(int value)
+                {
+                    if (MinStack.Count == 0 || value <= MinStack.Peek()) { MinStack.Push(value); }
+                    Stack.Push(value);
+                }
+
+                // The top of MinStack is popped off if it is equal to the value popped off of Stack
+                // (i.e., when the minimum value of Stack is being removed).
+                public int Pop()
+                {
+                    if (Stack.Count == 0) { } // Throw error
+                    var topOfStack = Stack.Pop();
+                    if (topOfStack == MinStack.Peek()) { MinStack.Pop(); }
+                    return topOfStack;
+                }
+
+                // The top element of MinStack is always the minimum value of Stack.
+                public int Min()
+                {
+                    if (MinStack.Count == 0) { } // Throw error
+                    return MinStack.Peek();
+                }
+            }
         }
     }
 }
