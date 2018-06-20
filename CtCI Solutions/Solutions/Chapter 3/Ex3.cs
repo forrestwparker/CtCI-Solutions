a292770eb0883bb411eeddc63ebf0d542ddbeddc
Forrest Parker <forrestwparker@gmail.com>
2018-06-20 11:00:21 -0700

    Ex 3.3 (No tests)

--- CtCI Solutions/Solutions/Chapter 3/Ex3.cs
+++ CtCI Solutions/Solutions/Chapter 3/Ex3.cs
@@ -28,7 +28,59 @@ namespace CtCI_Solutions.Solutions
              * Implement a function 'popAt(int index)' which performs a pop operation on a specific sub-stack.
              */
 
-            // <Code>
+            public class SetOfStacks<T>
+            {
+                private readonly List<Stack<T>> stackList = new List<Stack<T>> { };
+                private readonly int stackThreshold = 5;
+                public int Count { get; private set; } = 0;
+
+                private int stackIndexOfTop = -1;
+
+                public void Push(T item)
+                {
+                    if (
+                        stackIndexOfTop < 0
+                        || (stackList.Count > stackIndexOfTop && stackList[stackIndexOfTop].Count >= stackThreshold)
+                        )
+                    {
+                        stackIndexOfTop++;
+                    }
+                    if (stackList.Count <= stackIndexOfTop) { stackList.Add(new Stack<T>()); }
+                    stackList[stackIndexOfTop].Push(item);
+                    Count++;
+                }
+
+                public T Pop()
+                {
+                    if (stackIndexOfTop < 0) { throw new System.InvalidOperationException("Cannot pop from an empty stack."); }
+                    var item = stackList[stackIndexOfTop].Pop();
+                    Count--;
+                    SetTopOfStackIndex(stackIndexOfTop);
+                    return item;
+                }
+
+                public T PopAt(int index)
+                {
+                    if (index > stackIndexOfTop) { throw new System.ArgumentOutOfRangeException("index", "Cannot pop from nonexistant stack."); }
+                    T item;
+                    try
+                    {
+                        item = stackList[index].Pop();
+                    }
+                    catch (Exception ex) { throw new System.InvalidOperationException("Cannot pop from an empty stack", ex); }
+                    Count--;
+                    SetTopOfStackIndex(index);
+                    return item;
+                }
+
+                private void SetTopOfStackIndex(int indexPoppedFrom)
+                {
+                    if (indexPoppedFrom == stackIndexOfTop)
+                    {
+                        while (stackIndexOfTop >= 0 && stackList[stackIndexOfTop].Count == 0) { stackIndexOfTop--; }
+                    }
+                }
+            }
         }
     }
 }
