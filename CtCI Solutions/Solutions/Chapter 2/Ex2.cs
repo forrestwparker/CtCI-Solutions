29a7c100885e3bd97d8f386e5bc19e225332a9fe
Forrest Parker <forrestwparker@gmail.com>
2018-06-21 09:10:39 -0700

    Ex 2.2 (no tests)

--- CtCI Solutions/Solutions/Chapter 2/Ex2.cs
+++ CtCI Solutions/Solutions/Chapter 2/Ex2.cs
@@ -15,7 +15,30 @@ namespace CtCI_Solutions.Solutions
              * Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
              */
 
-            // <Code>
+            // Assumes last node is "0th to last."
+            // O(n) runtime, O(1) space
+            public static LinkedList.Node KToLast(LinkedList.Node head, int K)
+            {
+                if (head == null) { throw new System.ArgumentNullException("head"); }
+                var node = head;
+                for (int i = 0; i < K; i++)
+                {
+                    if (node.Next == null)
+                    {
+                        throw new System.InvalidOperationException(
+                            String.Format("LinkedList does not contain at least {0} nodes", K + 1)
+                        );
+                    }
+                    node = node.Next;
+                }
+                var backNode = head;
+                while (node.Next != null)
+                {
+                    backNode = backNode.Next;
+                    node = node.Next;
+                }
+                return backNode;
+            }
         }
     }
 }
