c5bd44a7e9d274e72f66cd0233a21cbee3a973e7
Forrest Parker <forrestwparker@gmail.com>
2018-06-22 12:25:33 -0700

    Ex 1.9 (no tests)

--- CtCI Solutions/Solutions/Chapter 1/Ex9.cs
+++ CtCI Solutions/Solutions/Chapter 1/Ex9.cs
@@ -13,11 +13,20 @@ namespace CtCI_Solutions.Solutions
             /* Exercise 1.9
              * 
              * String Rotation: Assume you have a method 'isSubstring' which checks if one word is a substring of another.
-             * Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
+             * Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to 'isSubstring'
              * (e.g., "waterbottle" is a rotation of "erbottlewat").
              */
 
-            // <Code>
+            // Assume no null strings.
+            // O(n * O(isSubstring)) runtime, o(n) space
+            public static bool IsRotation(string str1, string str2)
+            {
+                // If the strings aren't the same length, they cannot be rotations of each other.
+                if (str1.Length != str2.Length) { return false; }
+
+                // 'Contains' is the C# version of 'isSubstring'.
+                return (str1 + str1).Contains(str2);
+            }
         }
     }
 }
