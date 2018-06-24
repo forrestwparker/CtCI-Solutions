1368f1a1df5b557b334a4c63184dfdfb88c7d820
Forrest Parker <forrestwparker@gmail.com>
2018-06-24 11:35:35 -0700

    Ex 16.5 (no tests)

--- CtCI Solutions/Solutions/Chapter 16/Ex5.cs
+++ CtCI Solutions/Solutions/Chapter 16/Ex5.cs
@@ -14,7 +14,22 @@ namespace CtCI_Solutions.Solutions
              * 
              * Factorial Zeros: Write an algorithm which computes the number of trailing zeros in n factorial.
              */
-
+            
+            // Count the total number of factors of five for all integers between 1 and n.
+            // Do this by repeatedly dividing n by five, truncate the quotient, and add to count.
+            // O(log n) runtime, O(1) space
+            public static int ZerosInFactorial(int n)
+            {
+                if (n < 0) { throw new System.ArgumentOutOfRangeException("n", "Factorial is defined for non-negative numbers"); }
+                if (n < 5) { return 0; }
+                int count = 0;
+                do
+                {
+                    n = n / 5;
+                    count += n;
+                } while (n >= 5);
+                return count;
+            }
         }
     }
 }
