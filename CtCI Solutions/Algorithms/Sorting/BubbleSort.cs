39416f7242f5a40d2b9ebc4c0cad82897a52a1f8
Forrest Parker <forrestwparker@gmail.com>
2018-07-01 11:34:40 -0700

    Bubble sort (no test)

---
+++ CtCI Solutions/Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtCI_Solutions.Algorithms
+{
+    public static class Sorts
+    {
+        public static void BubbleSort(int[] array)
+        {
+            if (array == null) { throw new ArgumentNullException(); }
+            if (array.Length > 1)
+            {
+                bool didSwap;
+                do
+                {
+                    didSwap = false;
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        if (array[i] > array[i + 1])
+                        {
+                            var temp = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = temp;
+                            didSwap = true;
+                        }
+                    }
+                } while (didSwap);
+            }
+        }
+    }
+}
