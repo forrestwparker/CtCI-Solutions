7b14066a9b4adf8e8eba2185fbc7b13695d87023
Forrest Parker <forrestwparker@gmail.com>
2018-07-09 11:06:39 -0700

    Ex 4.5 (no test)

---
+++ CtCI Solutions/Solutions/Chapter 4/Ex5.cs
@@ -0,0 +1,40 @@
+﻿using CtCI_Solutions.Data_Structures;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CtCI_Solutions.Solutions
+{
+    public partial class Ch4 // Chapter number
+    {
+        public static class Ex5 // Exercise number
+        {
+            /* Exercise 4.5
+             * 
+             * Validate BST: Implement a function to check if a binary tree is a binary search tree.
+             */
+
+            // O(n) runtime, O(log n) space
+            public static bool ValidateBST(BinaryTreeNode<int> node)
+            {
+                if (node == null) { throw new ArgumentNullException(); }
+                return IsBST(node, null, null);
+            }
+
+            private static bool IsBST(BinaryTreeNode<int> node, int? minBound, int? maxBound)
+            {
+                if (node == null) { return true; }
+
+                if ((minBound != null && node.Data <= minBound)
+                    || ( maxBound != null && node.Data > maxBound)) { return false; }
+
+                if (!IsBST(node.LeftChild, minBound, node.Data) || !IsBST(node.RightChild, node.Data + 1, maxBound)) { return false; }
+
+                return true;
+            }
+        }
+    }
+}
